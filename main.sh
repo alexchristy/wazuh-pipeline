11c8decba87d5dbcf6f278bd65a47d199ee75eb3
Alex Christy <a.christy@ufl.edu>
2024-06-30 18:30:53 -0400

    Start wazuh server before running other scripts

--- main.sh
+++ main.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+echo "Starting wazuh manager..."
+/init &
+
 # Delay until the server is fully started
+echo "Waiting for wazuh manager to start up..."
 sh ./start_delay.sh
 
 # Install custom rules and decoders
+echo "Installing custom rules and decoders..."
 sh ./rule_decoder_installer.sh
