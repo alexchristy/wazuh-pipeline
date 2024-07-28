551d1e6c9b9496ed6b76db58969e802e1da2b010
Alex Christy <a.christy@ufl.edu>
2024-07-28 19:09:20 -0400

    Add support for private GitHub repositories

--- init.sh
+++ init.sh
@@ -2,8 +2,14 @@
 PIPELINE_REPO_PATH="/root/wazuh_pipeline"
 
 # Setup pipeline repo
-git clone "$REPO_URL" "$PIPELINE_REPO_PATH"
-cd "$PIPELINE_REPO_PATH" ||  1
+if [ -n "$GITHUB_TOKEN" ]; then
+  REPO_URL_WITH_TOKEN="https://${GITHUB_TOKEN}@${REPO_URL#https://}"
+  git clone "$REPO_URL_WITH_TOKEN" "$PIPELINE_REPO_PATH"
+else
+  git clone "$REPO_URL" "$PIPELINE_REPO_PATH"
+fi
+
+cd "$PIPELINE_REPO_PATH" || exit 1
 chmod +x ./*.sh
 
 # Check for the -i flag
