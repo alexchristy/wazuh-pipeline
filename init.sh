d2b1207ac3898145be8f39bfe4f2a164c9fe3b64
Alex Christy <a.christy@ufl.edu>
2024-07-28 19:41:53 -0400

    Add extra debugging statements to init.sh

--- init.sh
+++ init.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Define the pipeline repository path
 PIPELINE_REPO_PATH="/root/wazuh_pipeline"
 
-# Setup pipeline repo
+# Check if TOKEN is provided
 if [ -n "$TOKEN" ]; then
   REPO_URL_WITH_TOKEN="https://${TOKEN}@${REPO_URL#https://}"
+  echo "Cloning repository with token"
   git clone "$REPO_URL_WITH_TOKEN" "$PIPELINE_REPO_PATH"
 else
+  echo "Cloning repository without token"
   git clone "$REPO_URL" "$PIPELINE_REPO_PATH"
 fi
 
-cd "$PIPELINE_REPO_PATH" || exit 1
+# Navigate to the cloned repository directory
+cd "$PIPELINE_REPO_PATH"
+
+# Ensure all shell scripts are executable
 chmod +x ./*.sh
 
-# Check for the -i flag
+# Run the main script with the provided flag
 if [ "$1" = "-i" ]; then
   ./main.sh -i
 else
